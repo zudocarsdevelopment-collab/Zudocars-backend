41858165824e1bbb989ab1426eda4dbe1bea0e24
varuncareergize <varun.124150@gmail.com>
2026-08-05 19:23:40 +0530

    Test_up

--- zudobackend/settings.py
+++ zudobackend/settings.py
@@ -47,13 +47,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',   # <- move here
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # must be near the top, before CommonMiddleware
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'zudobackend.urls'
