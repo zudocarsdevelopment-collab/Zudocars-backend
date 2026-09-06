bf5bca253b88da204a5f9b0a230dd7d31c55a115
varuncareergize <varun.124150@gmail.com>
2026-09-06 22:21:09 +0530

    Fix login API authentication

--- authenticator/views.py
+++ authenticator/views.py
@@ -2,9 +2,13 @@ from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 
 class LoginAPIView(APIView):
+
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def post(self, request):
         email = request.data.get("email")
