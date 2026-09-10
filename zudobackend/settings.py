444c7fc9461ff00c6b79390329f3ea064594048b
varuncareergize <varun.124150@gmail.com>
2026-09-10 15:50:40 +0530

    stetings_up

--- zudobackend/settings.py
+++ zudobackend/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
