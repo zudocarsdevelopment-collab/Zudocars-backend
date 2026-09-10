99390f531437bc7295ba3eae043069542615a942
varuncareergize <varun.124150@gmail.com>
2026-09-10 09:19:45 +0530

    testeing

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import AvailableVehiclesAPIView,CreateEstimateBookingAPIView
 from .zudo_pdf_view import ZudoEstimatePDFAPIView
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 urlpatterns = [
     path('vehicles/available/', AvailableVehiclesAPIView.as_view(), name='available-vehicles'),
     path('estimates/create/', CreateEstimateBookingAPIView.as_view(), name='create-estimate'),
-    path('estimates/pdf/', ZudoEstimatePDFAPIView.as_view(), name='zudo-estimate-pdf'),  # ADD THIS
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('estimates/pdf/', ZudoEstimatePDFAPIView.as_view(), name='zudo-estimate-pdf'),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
