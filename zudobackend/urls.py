66503c2999e1ad08e700fae8ce185815e5981d07
varuncareergize <varun.124150@gmail.com>
2026-09-10 09:25:24 +0530

    test

--- zudobackend/urls.py
+++ zudobackend/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
+from django.urls import include, path, re_path
 from django.conf import settings
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authenticator.urls')),
     path('api/', include('fleet.urls')),
     path('api/', include('booking.urls')),
-    path('', include('authenticator.urls'))
- 
+    path('', include('authenticator.urls')),
+
+    # Serve /media/ regardless of DEBUG.
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
